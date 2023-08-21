33e0d3f32ce92a2858a6af2a3068a36e32607070
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-08-22 00:43:39 +0300

    j

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,6 +3,8 @@ import Slider from 'react-slick';
 import photoSlider__1 from '../../assets/slider/slider-1.jpg';
 import photoSlider__2 from '../../assets/slider/slider-2.jpg';
 import photoSlider__3 from '../../assets/slider/slider-3.jpg';
+import button__left from '../../assets/slider/buton-left.jpg';
+import button__right from '../../assets/slider/buton-right.jpg';
 import style from './Slider.module.scss';
 export default function SimpleSlider() {
   var settings = {
@@ -11,6 +13,16 @@ export default function SimpleSlider() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    prevArrow: (
+      <div class={style.button__left}>
+        <img src={button__left} />
+      </div>
+    ),
+    nextArrow: (
+      <div class={style.button__right}>
+        <img src={button__right} />
+      </div>
+    ),
   };
   return (
     <Slider className={style.Slider} {...settings}>
